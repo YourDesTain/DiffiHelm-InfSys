9b493d03d2fe5a7cfbe2904f4e4bb1dfce4079ad
YourDesTain <146116783+YourDesTain@users.noreply.github.com>
2023-11-05 11:00:41 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function randp() {
+    var randomValue = Math.floor(Math.random() * 1000) + 1; //числа от 1 до 1000
+    document.getElementById("p").value = randomValue;
+}
+
+function randg() {
+    var randomValue = Math.floor(Math.random() * 100) + 1; //числа от 1 до 100
+    document.getElementById("g").value = randomValue;
+}
+
+function randa() {
+    var randomValue = Math.floor(Math.random() * 50) + 1; //числа от 1 до 50
+    document.getElementById("a").value = randomValue;
+}
+
+function randb() {
+    var randomValue = Math.floor(Math.random() * 50) + 1; //числа от 1 до 50
+    document.getElementById("b").value = randomValue;
+}
+
 function calculateKey() {
 
     const p = parseInt(document.getElementById('p').value);
