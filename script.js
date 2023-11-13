85ae9b150109e626f8818c72d0578df77d465091
YourDesTain <146116783+YourDesTain@users.noreply.github.com>
2023-11-13 19:16:07 +0300

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
 function randp() {
-    var randomValue = Math.floor(Math.random() * 1000) + 1; //числа от 1 до 1000
+    var randomValue = Math.floor(Math.random() * 100000) + 1; //числа от 1 до 1000
     document.getElementById("p").value = randomValue;
 }
 
 function randg() {
-    var randomValue = Math.floor(Math.random() * 100) + 1; //числа от 1 до 100
+    var randomValue = Math.floor(Math.random() * 1000) + 1; //числа от 1 до 100
     document.getElementById("g").value = randomValue;
 }
 
 function randa() {
-    var randomValue = Math.floor(Math.random() * 50) + 1; //числа от 1 до 50
+    var randomValue = Math.floor(Math.random() * 250) + 1; //числа от 1 до 50
     document.getElementById("a").value = randomValue;
 }
 
 function randb() {
-    var randomValue = Math.floor(Math.random() * 50) + 1; //числа от 1 до 50
+    var randomValue = Math.floor(Math.random() * 250) + 1; //числа от 1 до 50
     document.getElementById("b").value = randomValue;
 }
 
-function calculateKey() {
+function compute(obj) {
+    var base = obj.base.value;
+    var modulus = obj.modulus.value;
 
+    var private_A = obj.private_A.value;
+    var public_A = discrete_exp(base, private_A, modulus);
+
+    var private_B = obj.private_B.value;
+    var public_B = discrete_exp(base, private_B, modulus);
+
+    obj.public_A.value = public_A;
+    obj.public_B.value = public_B;
+
+    obj.dh_A.value = discrete_exp(public_B, private_A, modulus);
+    obj.dh_B.value = discrete_exp(public_A, private_B, modulus);
+}
+
+function calculateKey() {
     const p = parseInt(document.getElementById('p').value);
     const g = parseInt(document.getElementById('g').value);
     const a = parseInt(document.getElementById('a').value);
     const b = parseInt(document.getElementById('b').value);
 
-    const A = Math.pow(g, a) % p;
-    const B = Math.pow(g, b) % p;
+    const A = discrete_exp(g, a, p);
+    const B = discrete_exp(g, b, p);
 
-    const sharedKeyA = Math.pow(B, a) % p;
-    const sharedKeyB = Math.pow(A, b) % p;
+    const sharedKeyA = discrete_exp(B, a, p);
+    const sharedKeyB = discrete_exp(A, b, p);
 
     document.getElementById('result').innerText = `Секретный ключ (Алиса): ${sharedKeyA}\nСекретный ключ (Боб): ${sharedKeyB}`;
+}
+
+function discrete_exp(base, exponent, modulus) {
+    let result = 1;
+    base = base % modulus;
+
+    while (exponent > 0) {
+        if (exponent % 2 === 1) {
+            result = (result * base) % modulus;
+        }
+
+        exponent = Math.floor(exponent / 2);
+        base = (base * base) % modulus;
+    }
+
+    return result;
 }
